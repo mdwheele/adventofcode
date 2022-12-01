008c29faaa4639eabaa1be7383684fddaaff0a8a
Dustin Wheeler <dustin.wheeler@nutanix.com>
2022-12-01 12:13:02 -0500

    Add comments

--- 2022/01/main.mjs
+++ 2022/01/main.mjs
@@ -4,11 +4,17 @@ import { readFileSync } from 'fs'
 const input = readFileSync('input').toString()
 
 const data = input
+  // Split the dataset by empty lines.
   .split(/\n\n/)
+  // Split the dataset into individual calorie strings.
   .map(dataset => dataset.split(/\n/))
+  // Sum up calorie counts per elf.
   .map(calories => calories.reduce((acc, count) => acc += Number(count), 0))
+  // Sort descending.
   .sort((a, b) => b - a)
 
-
+// Top elf.
 console.log('Part 1: ', data[0])
+
+// Top three elves.
 console.log('Part 2: ', data[0] + data[1] + data[2])
